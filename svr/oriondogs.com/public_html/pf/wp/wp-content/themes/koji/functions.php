b4f39dc87ddabb7a4568147682b3cd1eb44afc70
togane <i0maria@yahoo.co.jp>
2023-04-11 01:17:04 +0900

    8: ＜PF＞テーマ更新による問題点、全体再調整1

--- svr/oriondogs.com/public_html/pf/wp/wp-content/themes/koji/functions.php
+++ svr/oriondogs.com/public_html/pf/wp/wp-content/themes/koji/functions.php
@@ -871,3 +871,35 @@ if ( ! function_exists( 'koji_block_editor_styles' ) ) :
 	}
 	add_action( 'enqueue_block_editor_assets', 'koji_block_editor_styles', 1 );
 endif;
+
+
+/* ---------------------------------------------------------------------------------------------
+   投稿一覧に投稿IDを表示
+   --------------------------------------------------------------------------------------------- */
+add_filter('manage_posts_columns', 'posts_columns_id', 5);
+add_action('manage_posts_custom_column', 'posts_custom_id_columns', 5, 2);
+add_filter('manage_pages_columns', 'posts_columns_id', 5);
+add_action('manage_pages_custom_column', 'posts_custom_id_columns', 5, 2);
+function posts_columns_id($defaults){
+$defaults['wps_post_id'] = __('ID');
+return $defaults;
+}
+function posts_custom_id_columns($column_name, $id){
+if($column_name === 'wps_post_id'){
+echo $id;
+}
+}
+
+
+/* ---------------------------------------------------------------------------------------------
+   投稿記事の表示順を日付昇順（古→新）に変更
+   --------------------------------------------------------------------------------------------- */
+   function sort_postdata( $query ) {
+	   if ( is_admin() || ! $query->is_main_query() ) {
+		   return;
+	   }
+	   $query->set( 'order', 'ASC' );
+	   $query->set( 'orderby', 'date' );
+   }
+   
+   add_action( 'pre_get_posts', 'sort_postdata' );
